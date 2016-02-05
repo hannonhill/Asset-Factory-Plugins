f4a0012191a3dc78cf704ce283d525ae7b9b5cb6
Ryan Griffith <ryan.griffith@hannonhill.com>
2016-02-05 11:50:16 -0500

    Added new asset factory plugins. - Friendly Folder Name Plugin: restricts creation of folders based on whether the name matches a given regular expression - Publish Publish Set on Create Plugin: publishes the given Publish Set when the asset is created

---
+++ src/com/hannonhill/cascade/plugin/assetfactory/FriendlyFolderNamePlugin.java
@@ -0,0 +1,95 @@
+package com.hannonhill.cascade.plugin.assetfactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.cms.assetfactory.BaseAssetFactoryPlugin;
+import com.cms.assetfactory.PluginException;
+import com.hannonhill.cascade.api.asset.admin.AssetFactory;
+import com.hannonhill.cascade.api.asset.home.FolderContainedAsset;
+import com.hannonhill.cascade.model.dom.identifier.EntityTypes;
+import com.hannonhill.commons.util.StringUtil;
+
+/**
+ * This plugin is run to restrict creation of a Folder asset based on whether the asset's name matches the given regular 
+ * expression.
+ *
+ * @author Ryan Griffith
+ * @since 7.12.x
+ */
+public class FriendlyFolderNamePlugin extends BaseAssetFactoryPlugin
+{
+    private static final Logger LOG = getLogger(FriendlyFolderNamePlugin.class);
+    
+    private static final String DESCRIPTION_KEY = "plugin.assetfactory.friendlyfoldername.description";
+    private static final String NAME_KEY = "plugin.assetfactory.friendlyfoldername.name";
+
+    private static final String PARAM_NAMEREGEX_NAME_KEY = "plugin.assetfactory.friendlyfoldername.param.regex.name";
+    private static final String PARAM_NAMEREGEX_DESCRIPTION_KEY = "plugin.assetfactory.friendlyfoldername.param.regex.description";
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.BaseAssetFactoryPlugin#doPluginActionPre(com.hannonhill.cascade.api.asset.admin.AssetFactory, com.hannonhill.cascade.api.asset.home.FolderContainedAsset)
+     */
+    @Override
+    public void doPluginActionPre(AssetFactory factory, FolderContainedAsset asset) throws PluginException
+    {
+        //Does nothing before user edit.
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.BaseAssetFactoryPlugin#doPluginActionPost(com.hannonhill.cascade.api.asset.admin.AssetFactory, com.hannonhill.cascade.api.asset.home.FolderContainedAsset)
+     */
+    @Override
+    public void doPluginActionPost(AssetFactory factory, FolderContainedAsset asset) throws PluginException
+    {
+        LOG.debug("In doPluginActionPost, preparing to verify the name.");
+        if (EntityTypes.TYPE_FOLDER.equals(asset.getIdentifer().getType()))
+        {
+            String regex = getParameter(PARAM_NAMEREGEX_NAME_KEY);
+            String testString = asset.getName();
+            if (StringUtil.isNotEmpty(regex) && StringUtil.isNotEmpty(testString) && !testString.matches(regex))
+            {
+                setAllowCreation(false, "You may only give this folder a name that matches the following regular expression: " + regex);
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getDescription()
+     */
+    public String getDescription()
+    {
+        return DESCRIPTION_KEY;
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getName()
+     */
+    public String getName()
+    {
+        return NAME_KEY;
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getAvailableParameterDescriptions()
+     */
+    public Map<String, String> getAvailableParameterDescriptions()
+    {
+        Map<String, String> toRet = new HashMap<String, String>(1);
+        toRet.put(PARAM_NAMEREGEX_NAME_KEY, PARAM_NAMEREGEX_DESCRIPTION_KEY);
+        return toRet;
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getAvailableParameterNames()
+     */
+    public String[] getAvailableParameterNames()
+    {
+        return new String[]
+        {
+            PARAM_NAMEREGEX_NAME_KEY
+        };
+    }
+}
---
+++ src/com/hannonhill/cascade/plugin/assetfactory/PublishPublishSetOnCreatePlugin.java
@@ -0,0 +1,141 @@
+package com.hannonhill.cascade.plugin.assetfactory;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import com.cms.assetfactory.BaseAssetFactoryPlugin;
+import com.cms.assetfactory.PluginException;
+import com.hannonhill.cascade.api.asset.admin.AssetFactory;
+import com.hannonhill.cascade.api.asset.common.Identifier;
+import com.hannonhill.cascade.api.asset.common.Metadata;
+import com.hannonhill.cascade.api.asset.home.FolderContainedAsset;
+import com.hannonhill.cascade.api.asset.home.MetadataAwareAsset;
+import com.hannonhill.cascade.api.operation.Publish;
+import com.hannonhill.cascade.api.operation.Read;
+import com.hannonhill.cascade.api.operation.exception.ModelOperationException;
+import com.hannonhill.cascade.api.operation.exception.OperationValidationException;
+import com.hannonhill.cascade.api.operation.result.PublishOperationResult;
+import com.hannonhill.cascade.api.operation.result.ReadOperationResult;
+import com.hannonhill.cascade.model.dom.identifier.EntityType;
+import com.hannonhill.cascade.model.dom.identifier.EntityTypes;
+import com.hannonhill.commons.util.StringUtil;
+
+/**
+ * This plugin is run to automatically set the review date of an asset before the initial
+ * user edit screen is displayed, hence the user does not even need to worry about setting
+ * the review date if so desired.
+ *
+ * @author Ryan Griffith
+ * @since 7.4.x
+ */
+public class PublishPublishSetOnCreatePlugin extends BaseAssetFactoryPlugin
+{
+    private static final Logger LOG = getLogger(PublishPublishSetOnCreatePlugin.class);
+
+    private static final String DESCRIPTION_KEY = "plugin.assetfactory.publishpublishsetoncreate.description";
+    private static final String NAME_KEY = "plugin.assetfactory.publishpublishsetoncreate.name";
+
+    private static final String PARAM_PUBLISHSET_ID_NAME_KEY = "plugin.assetfactory.publishpublishsetoncreate.param.publishset.id.name";
+    private static final String PARAM_PUBLISHSET_ID_DESCRIPTION_KEY = "plugin.assetfactory.publishpublishsetoncreate.param.publishset.id.description";
+
+    /**
+     * An identifier
+     * 
+     * @author Ryan Griffith
+     */
+    private class IdentifierImpl implements Identifier
+    {
+        private final String id;
+        private final EntityType type;
+
+        public IdentifierImpl(String id, EntityType type)
+        {
+            this.id = id;
+            this.type = type;
+        }
+
+        public String getId()
+        {
+            return id;
+        }
+
+        public EntityType getType()
+        {
+            return type;
+        }
+    }
+    
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.BaseAssetFactoryPlugin#doPluginActionPre(com.hannonhill.cascade.api.asset.admin.AssetFactory, com.hannonhill.cascade.api.asset.home.FolderContainedAsset)
+     */
+    @Override
+    public void doPluginActionPre(AssetFactory factory, FolderContainedAsset asset) throws PluginException
+    {
+        //Does nothing before user edit.
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.BaseAssetFactoryPlugin#doPluginActionPost(com.hannonhill.cascade.api.asset.admin.AssetFactory, com.hannonhill.cascade.api.asset.home.FolderContainedAsset)
+     */
+    @Override
+    public void doPluginActionPost(AssetFactory factory, FolderContainedAsset asset) throws PluginException
+    {
+        String publishSetId = getParameter(PARAM_PUBLISHSET_ID_NAME_KEY);
+        if (StringUtil.isNotEmpty(publishSetId))
+        {
+            Publish publish = new Publish();
+            Identifier toRead = new IdentifierImpl(publishSetId, EntityTypes.TYPE_PUBLISHSET);
+            publish.setMode("publish");
+            publish.setToPublish(toRead);
+            publish.setUsername(getUsername());
+            try {
+                LOG.debug("Attempting to publish Publish Set: " + publishSetId);
+                publish.perform();
+            } catch (ModelOperationException e) {
+                LOG.debug("ModelOperationException: "+e.getMessage());
+            } catch (OperationValidationException e) {
+                LOG.debug("OperationValidationException: "+e.getMessage());
+            }
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getDescription()
+     */
+    public String getDescription()
+    {
+        return DESCRIPTION_KEY;
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getName()
+     */
+    public String getName()
+    {
+        return NAME_KEY;
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getAvailableParameterDescriptions()
+     */
+    public Map<String, String> getAvailableParameterDescriptions()
+    {
+        Map<String, String> toRet = new HashMap<String, String>(1);
+        toRet.put(PARAM_PUBLISHSET_ID_NAME_KEY, PARAM_PUBLISHSET_ID_DESCRIPTION_KEY);
+        return toRet;
+    }
+
+    /* (non-Javadoc)
+     * @see com.cms.assetfactory.AssetFactoryPlugin#getAvailableParameterNames()
+     */
+    public String[] getAvailableParameterNames()
+    {
+        return new String[]
+        {
+            PARAM_PUBLISHSET_ID_NAME_KEY
+        };
+    }
+}
